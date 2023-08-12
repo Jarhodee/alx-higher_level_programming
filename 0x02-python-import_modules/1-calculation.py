8e9f4ef469f6fcbc9ef1cf2eafe1548a473d0eb2
GaReN <christopherdezmond@gmail.com>
2023-08-12 05:02:43 +0100

    task 1. My first toolbox!

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Printing the difference, multiple, sum and quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
+
+    x = 10
+    z = 5
+
+    print("{} + {} = {}".format(x, z, add(x, z)))
+    print("{} - {} = {}".format(x, z, sub(x, z)))
+    print("{} * {} = {}".format(x, z, mul(x, z)))
+    print("{} / {} = {}".format(x, z, div(x, z))
