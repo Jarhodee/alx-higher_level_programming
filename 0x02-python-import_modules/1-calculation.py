deca381d3b09f4175f687fb199cad00f7f0f86c9
GaReN <christopherdezmond@gmail.com>
2023-08-15 09:55:18 +0100

    task 1 (a program that imports functions from the file calculator_1.py, does some Maths, and prints the result)

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     """Printing the difference, multiple, sum and quotient of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
-    x = 10
-    z = 5
+    a = 10
+    b = 5
 
-    print("{} + {} = {}".format(x, z, add(x, z)))
-    print("{} - {} = {}".format(x, z, sub(x, z)))
-    print("{} * {} = {}".format(x, z, mul(x, z)))
-    print("{} / {} = {}".format(x, z, div(x, z)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
