d7c23b6c610d841cedeb16207312a1dfcec9f0c3
GaReN <christopherdezmond@gmail.com>
2023-08-15 06:35:25 +0100

    task 1 a function that retrieves an element from a list like in C.

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a function that retrieves an element from a list like in C programming language."""
+
+
+def element_at(my_list, idx):
+    if dx < 0 or dx >= len(my_list):
+        return None
+    return my_list[dx]
