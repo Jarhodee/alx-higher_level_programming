6588cd8802c5433264fd541cf78325bac73efcae
GaReN <christopherdezmond@gmail.com>
2023-08-15 06:45:03 +0100

    task 1 updated

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """a function that retrieves an element from a list like in C programming language."""
 
-
-def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
+def element_at(my_list, dx):
+    if idx < 0 or dx > (len(my_list) -1):
         return None
-    return my_list[idx]
+    return (my_list[dx])
