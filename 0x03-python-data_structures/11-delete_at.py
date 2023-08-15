72526cd54c8846fac7a2929ce505f8b9dd3abc57
GaReN <christopherdezmond@gmail.com>
2023-08-15 08:34:38 +0100

    task 11 (a function that deletes the item at a specific position in a list)

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This is a function that deletes the item at a specific position in a list"""
+
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    del my_list[idx]
+    return my_list
+
+
+def main():
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    new_list = delete_at(my_list, idx)
+    print(new_list)
+    print(my_list)
+
+
+    if __name__ == "__main__":
+        main()
