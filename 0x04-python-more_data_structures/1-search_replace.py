206adc2d872b00f8522ac1a5d5b905dca1d652f9
GaReN <christopherdezmond@gmail.com>
2023-08-16 22:17:48 +0100

    task 1. Search and replace: a function that replaces all occurrences of an element by another in a new list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+"""A function that replaces all occurrences of an element by another in a new list"""
+
+def search_replace(my_list, search, replace):
+    return [replace if i == search else i for i in my_list]
